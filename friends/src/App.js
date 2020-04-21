7da0d52dccd96ee3a0ecc17fc5ec26418358bb45
Rachele Edwards <dot.com1472@gmail.com>
2020-04-21 14:32:20 -0600

    setup app, created app and installed dependencies. Set up App base as well as Login, axioswithauth and got functionality of login to work.

---
+++ friends/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
+import Login from "./components/Login"
+import Friends from "./components/Friends"
+import PrivateRoute from "./components/ProtectedRoute"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/login">Login</Link> &nbsp;
+            <Link to="/friends">Friends</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+          <PrivateRoute exact path="/friends" component={Friends} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
