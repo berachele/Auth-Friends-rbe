361f773fa25c76a9e6a880f8eca924888f43c556
Rachele Edwards <dot.com1472@gmail.com>
2020-04-21 15:59:58 -0600

    showing my friends--yaayy, now lets go ahead and add a buddy

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -14,7 +14,7 @@ class Friends extends React.Component {
         axiosWithAuth()
             .get("/api/friends")
             .then(res => {
-                // console.log({res})
+                console.log({res})
                 //res.data
                 this.setState({
                     friends: res.data
@@ -24,13 +24,16 @@ class Friends extends React.Component {
                 console.log("Error in Friends", err)
             })
     }
+
     render(){
         return(
             <div className="friends-wrapper">
                 <header>
                     <h1>F R I E N D S !</h1>
                 </header>
-                <p>List of friends here</p>
+                {this.state.friends.map(friend => {
+                    return <h3>{friend.name}</h3>
+                })}
             </div>
         )
     }
